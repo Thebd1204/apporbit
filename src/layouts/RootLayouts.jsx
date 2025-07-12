f0f5d43a881bdcb7875ba857e22a0112cea4e4e0
HossahinDev <hossahin321@gmail.com>
2025-07-12 19:34:36 +0600

    make project latouts

---
+++ src/layouts/RootLayouts.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Outlet, useNavigate } from 'react-router';
+import Navbar from '../Components/Navbar';
+import LoadingSpinners from './../Components/LoadingSpinners';
+import Footer from '../pages/Footer/Footer';
+
+const RootLayouts = () => {
+     const { state } = useNavigate();
+    return (
+       <div className="min-h-screen flex flex-col font-Poppins">
+      <header>
+        <div className="w-11/12 mx-auto">
+          <Navbar></Navbar>
+        </div>
+      </header>
+
+      <main className="flex-1">
+        <div className="w-11/12 mx-auto my-10">
+          {state === "loading" ? <LoadingSpinners /> : <Outlet></Outlet>}
+        </div>
+      </main>
+
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+    );
+};
+
+export default RootLayouts;
